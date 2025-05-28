d0cb0275ea398da02c97bdbe34cf77694ac034cc
AaliyanShuja <138379954+AaliyanShuja@users.noreply.github.com>
2025-05-28 18:36:46 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -199,7 +199,7 @@ st.markdown("""
 
 
 # FastAPI endpoint
-FASTAPI_URL = "https://salespredictor-production.up.railway.app/docs#/default/upload_csv_upload_csv_post" #/default/predict_batch_predict_batch_post  
+FASTAPI_URL = "https://salespredictor-production.up.railway.app/upload-csv" #/default/predict_batch_predict_batch_post  
 st.title("📊 Sales Prediction Dashboard")
 
 uploaded_file = st.file_uploader("📂 Upload CSV", type="csv")
